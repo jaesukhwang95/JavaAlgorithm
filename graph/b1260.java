a820a5652a9ad8535eb5300304a7894ee46c7032
HwangJaeSuk <jaesukhwang95@gmail.com>
2019-03-29 13:03:02 +0900

    graph

---
+++ graph/b1260.java
@@ -0,0 +1,76 @@
+package baekjoon.graph;
+import java.util.*;
+
+public class b1260 {
+//	4 5 1
+//	1 2
+//	1 3
+//	1 4
+//	2 4
+//	3 4
+	static ArrayList<Integer>[] a;
+	static boolean[] c;
+	
+	public static void dfs(int x)
+	{
+		if(c[x])
+			return;
+		c[x]=true;
+		System.out.print(x + " ");
+		for(int i: a[x])
+		{
+			if(!c[i])
+				dfs(i);
+		}
+		
+	}
+	
+	public static void bfs(int x)
+	{
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(x);
+		c[x] = true;
+		while(!q.isEmpty())
+		{
+			int y = q.remove();
+			System.out.print(y + " ");
+			for(int i: a[y])
+			{
+				if(!c[i])
+				{
+					c[i] = true;
+					q.add(i);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int start = sc.nextInt();
+		a = (ArrayList<Integer>[]) new ArrayList[n+1];
+		for(int i=1; i<=n; i++)
+		{
+			a[i] = new ArrayList<Integer>();
+		}
+		for(int i=0; i<m ;i++)
+		{
+			int u = sc.nextInt();
+			int v = sc.nextInt();
+			a[u].add(v);
+			a[v].add(u);
+		}
+		for(int i=1; i<=n; i++)
+		{
+			Collections.sort(a[i]);
+		}
+		c = new boolean[n+1];
+		dfs(start);
+		System.out.println();
+		c = new boolean[n+1];
+		bfs(start);
+	}
+}
--- graph/b13023.java
+++ graph/b13023.java
@@ -2,55 +2,56 @@ package baekjoon.graph;
 
 import java.util.*;
 
-class Edge {
-    int from, to;
-    Edge(int from, int to) {
-        this.from = from;
-        this.to = to;
-    }
-}
+
 public class b13023{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        boolean[][] a = new boolean[n][n];
-        @SuppressWarnings("unchecked")
-		ArrayList<Integer>[] g = (ArrayList<Integer>[]) new ArrayList[n];
-        ArrayList<Edge> edges = new ArrayList<Edge>();
-        for (int i=0; i<n; i++) {
-            g[i] = new ArrayList<Integer>();
-        }
-        for (int i=0; i<m; i++) {
-            int from = sc.nextInt();
-            int to = sc.nextInt();
-            edges.add(new Edge(from, to));
-            edges.add(new Edge(to, from));
-            a[from][to] = a[to][from] = true;
-            g[from].add(to);
-            g[to].add(from);
-        }
-        m *= 2;
-        for  (int i=0; i<m; i++) {
-            for (int j=0; j<m; j++) {
-                int A = edges.get(i).from;
-                int B = edges.get(i).to;
-                int C = edges.get(j).from;
-                int D = edges.get(j).to;
-                if (A == B || A == C || A == D || B == C || B == D || C == D) {
-                    continue;
-                }
-                if (!a[B][C]) continue;
-                for (int E : g[D]) {
-                    if (A == E || B == E || C == E || D == E) {
-                        continue;
-                    }
-                    System.out.println(1);
-                    System.exit(0);
-                }
-            }
-        }
-        System.out.println(0);
-        sc.close();
-    }
+		
+		static ArrayList<Integer>[] a;
+		static boolean [] c;
+		static boolean ans = false;
+		
+		public static void dfs(int x, int cnt)
+		{
+			if(cnt == 5)
+			{
+				ans = true;
+				return;
+			}
+			c[x] = true;
+			for(int i : a[x])
+			{
+				if(!c[i])
+					dfs(i, cnt+1);	
+			}		
+		}
+		
+	    public static void main(String[] args) {
+	    	Scanner sc = new Scanner(System.in);
+	    	int n = sc.nextInt();
+	    	int m = sc.nextInt();
+	    	a = (ArrayList<Integer>[]) new ArrayList[n];
+	    	for(int i=0; i<n; i++)
+	    	{
+	    		a[i] = new ArrayList<Integer>();
+	    	}
+	    	for(int i=0; i<m ;i++)
+	    	{
+	    		int u = sc.nextInt();
+	    		int v = sc.nextInt();
+	    		a[u].add(v);
+	    		a[v].add(u);
+	    	}
+	    	for(int i=0; i<n ;i++)
+	    		Collections.sort(a[i]);
+	    	for(int i=0; i<n ;i++)
+	    	{
+	    		c = new boolean[n];
+	    		dfs(i,1);
+	    		if (ans)
+	    		{
+	    			System.out.println(1);
+	    			System.exit(0);
+	    		}
+	    	}
+	    	System.out.println(0);
+	    }
 }
