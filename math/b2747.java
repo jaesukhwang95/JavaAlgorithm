9ae69a678df6b6b3a638067d193b0ffa128d08df
HwangJaeSuk <jaesukhwang95@gmail.com>
2019-12-17 23:16:17 +0900

    math

---
+++ math/b2747.java
@@ -0,0 +1,19 @@
+package baekjoon.math;
+
+import java.io.*;
+
+public class b2747 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n + 1];
+		arr[0] = 0;
+		arr[1] = 1;
+		if (n >= 2) {
+			for (int i = 2; i <= n; i++) {
+				arr[i] = arr[i - 1] + arr[i - 2];
+			}
+		}
+		System.out.println(arr[n]);
+	}
+}
