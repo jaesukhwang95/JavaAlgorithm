24aa40e637c61528f78561acbbc99377746ba3dd
HwangJaeSuk <jaesukhwang95@gmail.com>
2019-12-18 23:29:58 +0900

    math

--- math/b2747.java
+++ math/b2747.java
@@ -6,12 +6,13 @@ public class b2747 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n + 1];
+		long[] arr = new long[n + 1];
 		arr[0] = 0;
 		arr[1] = 1;
 		if (n >= 2) {
 			for (int i = 2; i <= n; i++) {
 				arr[i] = arr[i - 1] + arr[i - 2];
+				arr[i] = arr[i] % 1000000;
 			}
 		}
 		System.out.println(arr[n]);
