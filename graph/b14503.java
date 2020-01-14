5154fe2e028489f80cde47701793f8b5c1b4ae4c
HwangJaeSuk <jaesukhwang95@gmail.com>
2020-01-15 00:02:58 +0900

    graph

--- graph/b14503.java
+++ graph/b14503.java
@@ -3,16 +3,6 @@ package baekjoon.graph;
 import java.util.*;
 import java.io.*;
 
-//(r, c)로 나타낼 수 있고, r은 북쪽으로부터 떨어진 칸의 개수, c는 서쪽으로 부터 떨어진 칸의 개수이다.
-//d가 0인 경우에는 북쪽을, 1인 경우에는 동쪽을, 2인 경우에는 남쪽을, 3인 경우에는 서쪽을 바라보고
-
-// 왼쪽 1  오른쪽 3  위 0 아래 2
-
-// 1 -> 2 -> 3 -> 0
-// 왼 -> 아 -> 오 -> 위
-// 4방향 -> 후진
-// 후진 불가 시 stop
-
 public class b14503 {
 
 	public static class Pair {
@@ -29,19 +19,19 @@ public class b14503 {
 
 	public static Pair changeDirection(int r, int c, int dir, int[][] space, boolean[][] visit, int n, int m) {
 		for (int i = 0; i < 4; i++) {
-			dir = (dir + 1) % 4;
+			dir = (4 + dir - 1) % 4;
 			if (dir == 0) {
 				if (r - 1 >= 0 && !visit[r - 1][c] && space[r - 1][c] == 0)
 					return new Pair(r - 1, c, 0);
 			} else if (dir == 1) {
-				if (c - 1 >= 0 && !visit[r][c - 1] && space[r][c - 1] == 0)
-					return new Pair(r, c - 1, 1);
+				if (c + 1 < m && !visit[r][c + 1] && space[r][c + 1] == 0)
+					return new Pair(r, c + 1, 1);
 			} else if (dir == 2) {
 				if (r + 1 < n && !visit[r + 1][c] && space[r + 1][c] == 0)
 					return new Pair(r + 1, c, 2);
 			} else if (dir == 3) {
-				if (c + 1 < m && !visit[r][c + 1] && space[r][c + 1] == 0)
-					return new Pair(r, c + 1, 3);
+				if (c - 1 >= 0 && !visit[r][c - 1] && space[r][c - 1] == 0)
+					return new Pair(r, c - 1, 3);
 			}
 		}
 		if ((dir + 2) % 4 == 0) {
@@ -51,9 +41,9 @@ public class b14503 {
 
 			}
 		} else if ((dir + 2) % 4 == 1) {
-			if (c - 1 >= 0 && space[r][c - 1] == 0) {
-				visit[r][c - 1] = true;
-				return new Pair(r, c - 1, dir);
+			if (c + 1 < m && space[r][c + 1] == 0) {
+				visit[r][c + 1] = true;
+				return new Pair(r, c + 1, dir);
 			}
 		} else if ((dir + 2) % 4 == 2) {
 			if (r + 1 < n && space[r + 1][c] == 0) {
@@ -62,9 +52,9 @@ public class b14503 {
 
 			}
 		} else if ((dir + 2) % 4 == 3) {
-			if (c + 1 < m && space[r][c + 1] == 0) {
-				visit[r][c + 1] = true;
-				return new Pair(r, c + 1, dir);
+			if (c - 1 >= 0 && space[r][c - 1] == 0) {
+				visit[r][c - 1] = true;
+				return new Pair(r, c - 1, dir);
 			}
 		}
 		return new Pair(-1, -1, -1);
